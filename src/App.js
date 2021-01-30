124b83ab05e650ed69d7ed6cf0d076fd02afcb8b
Maahir18 <shilpisachdeva.wadhwa@gmail.com>
2021-01-30 18:53:04 -0500

    :

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
-import Experience from './Experience.js';
-import Contact from './Contact.js';
-import AboutMe from './AboutMe.js';
-import {Route,Switch} from 'react-router-dom';
-import {BrowserRouter} from 'react-router-dom';
+import Home from './components/Home';
+import Experience from './components/Experience.js';
+import Contact from './components/Contact.js';
+import AboutMe from './components/AboutMe.js';
+import Blogs from './Blogs/Blogs.js';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/Experience" component={Experience} />
       <Route exact path="/Contact" component={Contact} />
-      <Route exact path="/AboutMe" component={AboutMe} />      
+      <Route exact path="/AboutMe" component={AboutMe} />  
+      <Route exact path="/Blogs" component={Blogs} />     
     </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
